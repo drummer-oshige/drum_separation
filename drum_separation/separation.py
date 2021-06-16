bfb19d8e89484a9bf43e7b36d69402bb5f3e6a8d
drummer-oshige <drummer-oshige@gmail.com>
2021-06-16 16:21:27 +0900

    WIP.

--- drum_separation/separation.py
+++ drum_separation/separation.py
@@ -4,9 +4,9 @@ import soundfile
 
 def drum_separation(path):
     y, sr = librosa.load(path)
-    y_harmonic, y_percussive = librosa.effects.hpss(y, margin=1.0)
+    y_harmonic, y_percussive = librosa.effects.hpss(y, margin=1.5)
     return y_harmonic, y_percussive, sr
 
 
 def write(path, y, sr):
-    soundfile.write(path, y, sr, 'PCM_24')
+    soundfile.write(path, y, sr, format='wav', subtype='PCM_16')
