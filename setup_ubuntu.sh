23ba2cce10820282e50738bc22d027e411ca15a1
drummer-oshige <drummer-oshige@gmail.com>
2021-06-16 10:41:15 +0900

    WIP.

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -1,5 +1,4 @@
-python -m venv venv
-. venv/bin/activate
-pip3 install -r requirements_ubuntu.txt
-# pip3 install -r requirements_ubuntu.txt -t drum_separation
-
+# python3 -m venv venv
+# . venv/bin/activate
+# pip3 install -r requirements_ubuntu.txt
+pip3 install -r requirements_ubuntu.txt -t drum_separation
